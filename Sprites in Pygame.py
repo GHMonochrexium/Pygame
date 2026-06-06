01665a21f9cfbf4d06abf210246c11e2a1b8f2c6
GHMonochrexium <tycus_lawson@yahoo.com>
2026-06-06 16:23:32 +0100

    Add files via upload

--- Sprites in Pygame.py
+++ Sprites in Pygame.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+import time
 pygame.init()
-
+pygame.mixer.init()
 
 mainscreen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Sprites in Pygame")
@@ -25,10 +27,11 @@ ufo_sprite.center = (600, 600)
 meteor_sprite = img_meteor.get_rect()
 meteor_sprite.center = (200, 200)
 #KEYBINDS
-
-
+font1 = pygame.font.SysFont("Arial", 30)
+boom = pygame.mixer.music.load("vine_boom.mp3")
 
 run = True
+start_time = time.time()
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,15 +56,30 @@ while run:
         ufo_sprite.x = ufo_sprite.x - 2
     if keys[pygame.K_d]:
         ufo_sprite.x = ufo_sprite.x + 2
-
-
+    
     mainscreen.blit(bg_image, (0, 0))
     mainscreen.blit(img_rocket, rocket_sprite)
     mainscreen.blit(img_ufo, ufo_sprite)
     mainscreen.blit(img_meteor, meteor_sprite)
-
-     
-    pygame.display.update()
-
+    
+    if rocket_sprite.colliderect(ufo_sprite):
+        text1 = font1.render("Mission Complete!", True, "white")
+        pygame.mixer.music.play(1)   #since there is only one sound in this game atm, you dont have to specify.
+        rocket_sprite.center = (1100, 1000)
+        ufo_sprite.center = (1100, 1000)
+        meteor_sprite.center = (1000, 1000)
+        mainscreen.blit(text1, (300, 50))
+    current_time = time.time()
+    if current_time - start_time >= 5:
+       meteor_sprite.center = (random.randint(100, 700), random.randint(100, 700))
+       start_time = current_time
+    elif rocket_sprite.colliderect(meteor_sprite):
+        text1 = font1.render("Mission Failed!", True, "white")
+        pygame.mixer.music.play(1)   #since there is only one sound in this game atm, you dont have to specify.
+        rocket_sprite.center = (1100, 1000)
+        ufo_sprite.center = (1500, 1000)
+        meteor_sprite.center = (1100, 1000)
+        mainscreen.blit(text1, (300, 50))
+    pygame.display.update() 
 
 pygame.quit()
